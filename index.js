4f2869a065cd47c88c85326ec92a6c0181cd068d
Lrev47 <luis.revilla47@yahoo.com>
2023-11-06 13:00:03 -0600

    workshop complete

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+  const celsius = (fahren) -32 * 5/9;
+  return celsius
+}
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -22,14 +25,28 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  if (fahren > 32){
+    return `The temp is ${fahren}°F and ${celc}°C and it's very cold`
+  } else if (fahren < 64) {
+    return `The temp is ${fahren}°F and ${celc}°C and it's cold`
+  } else if (fahren < 86) {
+    return `The temp is ${fahren}°F and ${celc}°C and it's warm`
+  }else if (fahren < 100) {
+  return `The temp is ${fahren}°F and ${celc}°C and it's hot`
+  }
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+  const random = Math.random();
+  const numrange = Math.floor(random * (limit + 1));
+  return numrange
+}
 
 // -------------------- DO NOT CHANGE THE CODE BELOW ---------------------- //
 
